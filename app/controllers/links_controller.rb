6fc5d254c1e697e2ca19c6ad3839309ac822c26f
Caitlin O'Halloran <cohalls@gmail.com>
2018-10-12 16:14:39 +0000

    Edit, Update, Create, Show Actions and created views

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,14 +2,23 @@ class LinksController < ApplicationController
   def index
     @links = Link.all
   end
-
+  def show
+    @link = Link.find(params[:id])
+  end
   def new
     @link = Link.new
   end
   def create
     @link = Link.create(link_params)
   end
-
+  def edit
+    @link = Link.find(params[:id])
+  end
+  def update
+    @link = Link.find(params[:id])
+    @link.update(link_params)
+    redirect_to link_path(@link)
+  end
   private
 
   def link_params
