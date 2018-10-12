c4572ebf54ffe863a3122ad0791667d3a46d8bd1
Caitlin O'Halloran <cohalls@gmail.com>
2018-10-12 15:43:53 +0000

    Simple Form Gem and Created Form for Links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,4 +2,17 @@ class LinksController < ApplicationController
   def index
     @links = Link.all
   end
+
+  def new
+    @link = Link.new
+  end
+  def create
+    @link = Link.create(link_params)
+  end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:title, :url)
+  end
 end
