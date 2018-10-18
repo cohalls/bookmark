1c4d25c7fb15c47fa8e8b8b4cf0148687053313e
Caitlin O'Halloran <cohalls@gmail.com>
2018-10-18 19:06:53 +0000

    Added Destroy Action and Link

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -7,9 +7,11 @@ class LinksController < ApplicationController
   end
   def new
     @link = Link.new
+    redirect_to link_path
   end
   def create
     @link = Link.create(link_params)
+    redirect_to link_path
   end
   def edit
     @link = Link.find(params[:id])
@@ -18,6 +20,11 @@ class LinksController < ApplicationController
     @link = Link.find(params[:id])
     @link.update(link_params)
     redirect_to link_path(@link)
+  end
+  def destroy
+    @link = Link.find(params[:id])
+    @link.destroy
+    redirect :back
   end
   private
 
