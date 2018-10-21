0bec84fbe7a435b029a34f06554e0cac53a35727
Caitlin O'Halloran <cohalls@gmail.com>
2018-10-21 17:28:32 +0000

    Installed Devise Gem for User Sign In

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,30 +2,36 @@ class LinksController < ApplicationController
   def index
     @links = Link.all
   end
+
   def show
     @link = Link.find(params[:id])
   end
+
   def new
     @link = Link.new
-    redirect_to link_path
   end
+
   def create
     @link = Link.create(link_params)
-    redirect_to link_path
+    redirect_to links_path
   end
+
   def edit
     @link = Link.find(params[:id])
   end
+
   def update
     @link = Link.find(params[:id])
     @link.update(link_params)
     redirect_to link_path(@link)
   end
+
   def destroy
     @link = Link.find(params[:id])
     @link.destroy
-    redirect :back
+    redirect_to links_path
   end
+  
   private
 
   def link_params
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,10 +1,25 @@
 class TagsController < ApplicationController
   def index
-    @tags = Tags.all
+    @tags = Tag.all
   end
 
   def new
-
+    @tag = Tag.new
   end
 
+  def create
+    @tag = Tag.create(tag_params)
+    redirect_to links_path
+  end
+
+  def destroy
+    @tag = Tag.destroy(params[:id])
+    redirect_to tags_path
+  end
+
+  private
+
+  def tag_params
+      params.require(:tag).permit(:title)
+  end
 end
