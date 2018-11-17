f6064ef3e383a7de890df1698fcd11d608aa0ac7
Caitlin O'Halloran <cohalls@gmail.com>
2018-11-17 21:15:42 +0000

    Ability to Crawl Link URL for Images

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,12 @@
+require 'link_thumbnailer'
 class Link < ApplicationRecord
   has_and_belongs_to_many :tags
   belongs_to :user
+  before_save :crawl_page
+
+  def crawl_page
+    page = LinkThumbnailer.generate(self.url)
+    image = page.images.first.src.to_s
+    self.image_path = image
+  end
 end
