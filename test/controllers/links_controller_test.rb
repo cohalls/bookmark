4554e36736d69f09d2292af454c9bab486251131
Caitlin O'Halloran <cohalls@gmail.com>
2018-12-19 15:19:49 +0000

    Added FactoryBot gem. Changed CSS for Show Link Page and added link and link symbol to Index page

--- test/controllers/links_controller_test.rb
+++ test/controllers/links_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class LinksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  describe "GET index" do
+    it "assigns @links" do
+      link = Link.create
+      get :index
+      expect(response).to have_http_status :success
+    end
+  end
+
+  describe "links#create" do
+    it "should allow new links to be created" do
+      post :create, params: {link: {title: "Bookmark Title"}}
+      expect(response).to have_http_status(:success)
+      expect(Link.last.title).to eq("Bookmark Title")
+    end
+  end
 end
