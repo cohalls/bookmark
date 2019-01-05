901492a1481137547fb8d4e16b079cfa597d3f05
Caitlin O'Halloran <cohalls@gmail.com>
2019-01-05 17:25:50 +0000

    Completed Basic Search for Tags

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,18 @@
 class SearchController < ApplicationController
 
   def search
-
+    @tag = Tag.find_by(title: params[:tag], user: current_user)
+    if @tag
+      @links = @tag.links
+      render "links/index"
+    else
+      flash[:error] = "There are no Links with the Tag #{params[:tag]}"
+      @links = Link.all
+      render "links/index"
+    end
   end
 
   def search_params
-    params.require(:link).permit(:title, :url, :tags => [])
+    params.require(:tag)
   end
 end
