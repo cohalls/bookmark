b9cd8befc8ead1c972827270ed770f33bd1c3280
Caitlin O'Halloran <cohalls@gmail.com>
2019-01-06 22:07:05 +0000

    Added a Tag spec test

--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -22,5 +22,33 @@ RSpec.describe LinksController, type: :controller do
       expect(response).to redirect_to links_path
       expect(Link.last.title).to eq("Bookmark Title")
     end
+
+    it "should create a new link with a new tag" do
+      user = FactoryBot.create(:user)
+      sign_in user
+
+      post :create, params: {
+        link: { title: "Bookmark Title",
+                url: "www.bookmark.com",
+                tags: ['Cooking']
+            }
+          }
+        expect(response).to redirect_to links_path
+        expect(Link.last.title).to eq ("Bookmark Title")
+        expect(Link.last.tags.length).to eq (1)
+        expect(Link.last.tags[0].title).to eq("Cooking")
+    end
+
+    it "should create a new link with an existing tag" do
+    end
+  end
+
+  describe "links#edit" do
+    it "should add a new tag to an existing link" do
+    end
+
+    it "should add an existing tag to an existing link" do
+    end
+
   end
 end
